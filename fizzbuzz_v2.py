1b8b31cb4b7186a97df8615ead860657e3ea1a0a
div110 <davidxhalamka@gmail.com>
2022-01-03 21:56:56 +0100

    Add files via upload

---
+++ fizzbuzz_v2.py
@@ -0,0 +1,20 @@
+import time
+printing = 0
+num = 0
+while True:
+    printing = 0
+    num = num + 1
+    if num%15  == 0:
+        print('FizzBuzz')
+        printing = 1
+    elif num % 5 == 0:
+        if printing == 0:
+            print('Buzz')
+            printing = 1
+    elif num % 3 == 0:
+        if printing == 0:
+            print('Fizz')
+            printing = 1
+    elif printing == 0:
+        print(num)
+    time.sleep(0.5)
